775e41e58254d959dcaef1a24b2a459ced6db4a2
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-10-11 03:25:51 +0530

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () =>
+  <div className="footer hidden-mobile">
+    <a href="https://github.com/Neilblaze/speakit" target="_blank" rel="noopener noreferrer">
+      view source
+    </a>
+    <a href="https://twitter.com/Neilzblaze007" target="_blank" rel="noopener noreferrer">
+      @Neilzblaze007
+    </a>
+  </div>;
+
+export default Footer;
